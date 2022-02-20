85a81c63ec009155de011d890b0d34c641246c07
Brian Seo <98533321+BrianYSeo@users.noreply.github.com>
2022-02-20 22:38:22 +0000

    Completed

--- main.py
+++ main.py
@@ -18,9 +18,45 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. your code goes here
+michelangelo.forward(random.randrange(1,100))
+leonardo.forward(random.randrange(1,100))
+michelangelo.goto(-100, 20)
+leonardo.goto(-100, -20)
 
-
+for i in range(10):
+  leonardo.forward(random.randrange(1,10))
+  michelangelo.forward(random.randrange(1,10))
+michelangelo.goto(-100, 20)
+leonardo.goto(-100, -20)
 # Part B - complete part B here
+leonardo.down()
+for i in range(3):
+  leonardo.forward(25)
+  leonardo.right(120)
+leonardo.clear()
 
+leonardo.down()
+for i in range(4):
+  leonardo.forward(25)
+  leonardo.right(90)
+leonardo.clear()
+
+leonardo.down()
+for i in range(6):
+  leonardo.forward(25)
+  leonardo.right(60)
+leonardo.clear()
+
+leonardo.down()
+for i in range(9):
+  leonardo.forward(25)
+  leonardo.right(40)
+leonardo.clear()
+
+leonardo.down()
+for i in range(12):
+  leonardo.forward(25)
+  leonardo.right(30)
+leonardo.clear()
 
 window.exitonclick()
